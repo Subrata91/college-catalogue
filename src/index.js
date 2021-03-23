f2604219b8bd80ebab7ecb4d189909b6161befdb
Subrata Mondal <subrataron916@gmail.com>
2021-03-24 01:58:20 +0530

    Edited the code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
+//Importing of installed libraries and their components
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from 'react-router-dom';
 import axios from "axios";
 import {makeStyles} from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
@@ -9,10 +9,9 @@ import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import {Grid} from "@material-ui/core";
-import Routes from './Routes';
-import history from "./history";
 import "./styles.css";
 
+//Styling parameters of the card-grid and its constituent cards
 const useStyles = makeStyles({
   gridContainer: {
     paddingLeft: "40px",
@@ -34,10 +33,14 @@ const useStyles = makeStyles({
   }
 });
 
-export default function App() {
-  const [colleges, setcolleges] = useState(null);
-  const classes = useStyles();
 
+//This function performs the data fetching operation from the respective APIs
+function App() {
+  const [colleges, setcolleges] = useState(null); //This will fetch from the API of all the colleges
+  const [singleCollege, setsingleCollege] = useState(null); //This will fetch from the API of an individual college
+  const classes = useStyles(); //stores the above given stylesheet
+
+//Fetching data about all the colleges in one process
   const fetchData = async () => {
     const response = await axios.get(
       "/colleges"
@@ -46,12 +49,13 @@ export default function App() {
     setcolleges(response.data);
   };
 
+//Rendering of the home site
   return (
     <div className = "App" >
     <h1 > List of Colleges < /h1>
     <h2 > Fetch a list from an API and display it < /h2>
 
-    {/* Fetch data from API */ }
+    // Fetch data from API
     <div >
     <button className = "fetch-button" onClick = {fetchData} >
     Fetch Data
@@ -59,11 +63,20 @@ export default function App() {
     <br / >
     </div>
 
-    {/* Display data from API */ }
+    // Display data from API
     <div className = "colleges" >
     {colleges && colleges.map((college, index) => {
         const cleanedDate = new Date(college.Timestamp).toDateString();
 
+        const fetchData2 = async (pointer) => {
+          const response2 = await axios.get(
+            `{/college/${colleges[pointer]._id}}`
+          );
+
+          setsingleCollege(response2.data);
+        };
+
+        //Generation of the card-grid and the cards
         return (
             <Grid item xs = {12} sm = {6} md = {4} >
               <Card className = {classes.root} variant = "outlined" >
@@ -88,7 +101,38 @@ export default function App() {
                   </div>
                 </CardContent>
                 <CardActions >
-                  <Button size = "small" onClick={() => history.push('/SingleCollege')} > View In Detail < /Button>
+                  <Button size = "small" onClick={() => fetchData2(index)} > View In Detail < /Button> //When clicked on the button it will fetch data about only the clicked college
+                  <div className="singleCollege">
+                    {
+                      //Mapping through the data-items of only the clicked college
+                      singleCollege && singleCollege.map((institute, index) => {
+                        const cleanedDate2 = new Date(institute.Timestamp).toDateString();
+
+                        //Projecting the data regarding only the clicked college
+                        return(
+                          <div>
+                            <p><strong>ID:</strong> {institute._id}</p>
+                            <p><strong>Timestamp:</strong> {cleanedDate2}</p>
+                            <p><strong>Name:</strong> {institute.Name}</p>
+                            <p><strong>Address:</strong> {institute.Address}</p>
+                            <p><strong>Recognition:</strong> {institute.Recognition}</p>
+                            <p><strong>Accreditation:</strong> {institute.Accreditation}</p>
+                            <p><strong>Established:</strong> {institute.Established}</p>
+                            <p><strong>About:</strong> {institute.About}</p>
+                            <p><strong>Streams:</strong> {institute.Streams}</p>
+                            <p><strong>Placements:</strong> {institute.Placements}</p>
+                            <p><strong>Current Rankings:</strong> {institute.CurrentRankings}</p>
+                            <p><strong>Ranking:</strong> {institute.Ranking}</p>
+                            <p><strong>Gallery:</strong> {institute.Gallery}</p>
+                            <p><strong>Scholarships:</strong> {institute.Scholarships}</p>
+                            <p><strong>Faculty:</strong> {institute.Faculty}</p>
+                            <p><strong>Facilities:</strong> {institute.Facilities}</p>
+                            <p><strong>Contact Details:</strong> {institute.Contact}</p>
+                          </div>
+                        );
+                      }
+                    }
+                  </div>
                 </CardActions>
               </Card>
             </Grid>
@@ -101,7 +145,4 @@ export default function App() {
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <Router>
-    < App / >
-  </Router>, rootElement);
+ReactDOM.render(< App / >, rootElement);
