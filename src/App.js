cc30e779b6b9ccd90cf10f3ffc2068685412a020
Subrata Mondal <subrataron916@gmail.com>
2021-03-26 06:35:13 +0530

    Furthermore, uploading new code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import axios from "axios";
+import history from './history';
 import {makeStyles} from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -9,6 +10,7 @@ import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import {Grid} from "@material-ui/core";
 import "./styles.css";
+import SingleCollege from "./SingleCollege";
 
 const useStyles = makeStyles({
   gridContainer: {
@@ -43,6 +45,10 @@ function App() {
     setcolleges(response.data);
   };
 
+  const handler = function(ID){
+    return ID;
+  };
+
   return (
     <div className = "App" >
     <h1 > List of Colleges < /h1>
@@ -62,7 +68,6 @@ function App() {
         const cleanedDate = new Date(college.Timestamp).toDateString();
 
         return (
-          <Grid container spacing = {4} className = {classes.gridContainer} justify = "center" >
             <Grid item xs = {12} sm = {6} md = {4} >
               <Card className = {classes.root} variant = "outlined" >
                 <CardContent >
@@ -86,17 +91,17 @@ function App() {
                   </div>
                 </CardContent>
                 <CardActions >
-                  <Button size = "small" onClick = {alert(college._id)}> View In Detail < /Button>
+                  <Button size = "small" onClick = {"() => history.push('/SingleCollege'); () => handler(college._id);"}> View In Detail < /Button>
                 </CardActions>
               </Card>
             </Grid>
-          </Grid>
         );
       })
     }
     </div>
   </div>
   );
+  return(<SingleCollege id = {handler} />);
 }
 
 export default App;
--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Router, Switch, Route } from "react-router-dom";
+import App from "./App";
 import SingleCollege from "./SingleCollege";
 import history from './history';
 
@@ -8,6 +9,7 @@ export default class Routes extends Component {
         return (
             <Router history={history}>
                 <Switch>
+                    <Route path="/Home" component={App} />
                     <Route path="/SingleCollege" component={SingleCollege} />
                 </Switch>
             </Router>
